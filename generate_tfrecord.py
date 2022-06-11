e7e72b4cbb20499756e864019ae77afba4cfe157
JosephLiang05 <87677796+JosephLiang05@users.noreply.github.com>
2022-06-12 01:13:57 +0800

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -14,6 +14,7 @@ import pandas as pd
 import tensorflow as tf
 import sys
 # sys.path.append("/content/models/research")
+sys.path.append("/content/workspace/training/images")
 
 from PIL import Image
 from object_detection.utils import dataset_util
